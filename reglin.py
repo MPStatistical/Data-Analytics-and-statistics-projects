982352aa2462778514e20f6b03ac8526bf8fd925
Miguel Pardo <134332359+MPStatistical@users.noreply.github.com>
2023-06-04 13:45:28 -0500

    Update reglin.py

--- reglin.py
+++ reglin.py
@@ -3,9 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-from scipy.stats import shapiro, kstest, normaltest
+from scipy.stats import shapiro, kstest
 from statsmodels.stats.diagnostic import het_white
-import numpy as np
 
 # Configuración de estilo
 plt.style.use('seaborn-whitegrid')
@@ -33,32 +32,38 @@ if len(x_values) > 1 and len(y_values) > 1 and len(x_values) == len(y_values):
     # Realizar la regresión lineal
     results = smf.ols('y ~ x', data=data).fit()
 
+    # Obtener los valores ajustados y los intervalos de predicción
+    pred_vals = results.get_prediction().summary_frame()
+    lower_pred = pred_vals['obs_ci_lower']
+    upper_pred = pred_vals['obs_ci_upper']
+
     # Crear los subplots
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
 
-    # Visualizar la línea de regresión y los datos
+    # Visualizar la línea de regresión, los datos y los intervalos de predicción
     axs[0].scatter(data['x'], data['y'], label='Datos', color='skyblue', edgecolor='gray', alpha=0.8)
     axs[0].plot(data['x'].to_numpy(), results.fittedvalues.to_numpy(), color='red', label='Regresión Lineal')
+    axs[0].plot(data['x'].to_numpy(), lower_pred, color='gray', linestyle='--')
+    axs[0].plot(data['x'].to_numpy(), upper_pred, color='gray', linestyle='--')
     axs[0].set_xlabel('x')
     axs[0].set_ylabel('y')
     axs[0].set_title('Regresión Lineal')
     axs[0].legend(loc='upper left')
-
     # Visualizar los residuos con un QQ-Plot
-    sm.qqplot(results.resid, line='s', ax=axs[1])
-    axs[1].set_title('QQ-Plot de los residuos')
+    sm.qqplot(results.resid, line='s', ax=axs[1], color='gray')
+    axs[1].set_title('QQ-Plot')
 
     # Visualizar los residuos estandarizados frente a los valores ajustados
     residuals_standardized = results.resid_pearson
-    axs[2].scatter(results.fittedvalues, residuals_standardized, color='skyblue', edgecolor='gray', alpha=0.8)
-    axs[2].axhline(0, color='red', linestyle='--')  # Agregar línea horizontal
+    axs[2].scatter(results.fittedvalues, residuals_standardized, color='blue', edgecolor='gray', alpha=0.8)
+    axs[2].axhline(0, color='gray', linestyle='--')  # Agregar línea horizontal
     axs[2].set_xlabel('Valores ajustados')
     axs[2].set_ylabel('Residuos estandarizados')
     axs[2].set_title('Residuos estandarizados vs Valores ajustados')
 
     # Ajustar automáticamente el layout
     plt.tight_layout()
-    
+
     # Mostrar los gráficos
     st.pyplot(fig)
 
@@ -70,7 +75,7 @@ if len(x_values) > 1 and len(y_values) > 1 and len(x_values) == len(y_values):
     else:
         _, p_normal = kstest(residuals, 'norm')
         test_used = "Kolmogorov-Smirnov"
-    
+
     # Prueba de homocedasticidad (Prueba de White)
     _, p_white, _, _ = het_white(residuals, results.model.exog)
 
@@ -79,7 +84,7 @@ if len(x_values) > 1 and len(y_values) > 1 and len(x_values) == len(y_values):
 
     # Mostrar los resultados de las pruebas
     st.subheader("Resultados de las pruebas")
-    
+
     if p_normal > 0.05:
         st.write(f'Prueba de {test_used}: Los residuos siguen una distribución normal (p={p_normal:.3f})')
     else:
@@ -91,6 +96,10 @@ if len(x_values) > 1 and len(y_values) > 1 and len(x_values) == len(y_values):
         st.write(f'Prueba de White: Los errores no son homocedásticos (p={p_white:.3f})')
 else:
     st.write("Ingrese al menos 2 valores válidos para x e y.")
+
+
+
+
 
 
 
